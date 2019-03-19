4050add101684f668321b4a8720dbfaad4ed4448
michaelmcmullin <m.mcmullin@outlook.com>
2019-03-19 07:08:04 +0000

    Populate home with available dashboards

--- index.php
+++ index.php
@@ -7,5 +7,27 @@ include 'code/header.php'
         echo ' '.$user->name;
     }
 ?>!</h2>
+
+<?php
+if (!isset($user)) {
+    echo '<p>To access this site, please <a href="login.php">log in</a>.</p>';
+} else {
+    if (isset($user->dashboards) && count($user->dashboards) > 0) {
+        echo "<p>You have access to the following pages:</p>";
+        echo "\n<dl>";
+        foreach ($user->dashboards as $dashboard) {
+            echo "\n";
+            echo '<dt><a href="'.$dashboard->url;
+            echo '">'.$dashboard->name.'</a></dt>';
+            echo '<dd>'.$dashboard->description.'</dd>';
+        }
+        echo "\n</dl>";
+    } else {
+        echo "<p>It appears you haven't been given access to any pages yet. ";
+        echo "Please contact the administrator to get set up.</p>";
+    }
+}
+?>
+
 <?php include 'code/footer.php'
 ?>

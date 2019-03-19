a0d550059c9b461f5feeee5c9b29e66949d970d9
Michael McMullin <m.mcmullin@mcgowansprint.com>
2019-03-19 10:41:01 +0000

    Styles the home page dashboard

--- index.php
+++ index.php
@@ -14,14 +14,22 @@ if (!isset($user)) {
 } else {
     if (isset($user->dashboards) && count($user->dashboards) > 0) {
         echo "<p>You have access to the following pages:</p>";
-        echo "\n<dl>";
+        echo '<div class="row">';
         foreach ($user->dashboards as $dashboard) {
             echo "\n";
-            echo '<dt><a href="'.$dashboard->url;
-            echo '">'.$dashboard->name.'</a></dt>';
-            echo '<dd>'.$dashboard->description.'</dd>';
+            echo '<div class="col-xs-12 col-sm-6 col-md-4 mb-4">';
+            echo '<div class="card h-100">';
+            echo '<h5 class="card-header text-light bg-secondary">'.$dashboard->name.'</h5>';
+            echo '<div class="card-body">';
+            echo '<p class="card-text">'.$dashboard->description.'</p>';
+            echo '</div>';
+            echo '<div class="card-footer bg-white border-top-0 text-right">';
+            echo '<a href="'.$dashboard->url.'" class="btn btn-secondary">Launch</a>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
         }
-        echo "\n</dl>";
+        echo '</div>';
     } else {
         echo "<p>It appears you haven't been given access to any pages yet. ";
         echo "Please contact the administrator to get set up.</p>";

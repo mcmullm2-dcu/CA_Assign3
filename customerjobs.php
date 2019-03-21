84d9aa3d751cf47e344ad8a4b76b48f6d316bef3
Michael McMullin <m.mcmullin@mcgowansprint.com>
2019-03-21 16:11:45 +0000

    Displays logged in user's customers

--- customerjobs.php
+++ customerjobs.php
@@ -15,7 +15,49 @@ if (!isset($dashboard)) {
 
 echo '<h2>'.$dashboard->name.'</h2>';
 echo '<p>'.$dashboard->description.'</p>';
-?>
 
-<?php include 'code/footer.php'
+echo '<div class="row">';
+
+$customerDb = $db->GetCustomerDB();
+//$userDb = $db->GetUserDB();
+//$users = $userDb->getUsers(null);
+
+// URL without query string, from https://stackoverflow.com/a/6975045/5233918
+$edit_link = strtok($_SERVER["REQUEST_URI"], '?');
+
+// Main list of customers
+if (isset($user)) {
+    $customers = $customerDb->listCustomers($user);
+    echo '<div class="col-sm-12">';
+
+    if (count($customers) > 0) {
+        echo '<table class="table table-striped">';
+        echo '<thead><tr>';
+        echo '<th scope="col">Code</th>';
+        echo '<th scope="col">Customer Name</th>';
+        echo '<th scope="col">Account Manager</th>';
+        echo '<th scope="col">Status</th>';
+        echo '</tr></thead>';
+
+        echo '<tbody>';
+
+        foreach ($customers as $customer) {
+            echo '<tr>';
+            echo '<td>'.$customer->code.'</td>';
+            echo '<td>'.$customer->name.'</td>';
+            echo '<td>'.$customer->accountManager->name.'</td>';
+            echo '<td>-</td>';
+            echo '</tr>';
+        }
+
+        echo '</tbody>';
+        echo '</table>';
+ 
+    } else {
+        echo "<em>Sorry, you don't appear to be managing any customers</em>";
+    }
+    echo '</div>';
+}
+
+include 'code/footer.php';
 ?>

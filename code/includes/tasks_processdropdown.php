164a6efb1ef0c8d989c7aec38713e59ea87cd16d
michaelmcmullin <m.mcmullin@outlook.com>
2019-03-24 19:48:55 +0000

    Change task process via select box
    Uses jQuery if available to hide submit and use select instead.

--- code/includes/tasks_processdropdown.php
+++ code/includes/tasks_processdropdown.php
@@ -9,7 +9,7 @@
  */
 
 if (count($processes) > 1) {
-    echo '<form method="POST" class="form-inline mb-4">';
+    echo '<form method="POST" class="form-inline mb-4" id="process_chooser">';
     echo '<label for="select_process" class="mr-1">Select a process: </label>';
     echo '<select id="select_process" name="select_process" class="form-control mr-1">';
     foreach ($processes as $process) {
@@ -20,6 +20,15 @@ if (count($processes) > 1) {
         echo '>'.$process->name.'</option>';
     }
     echo '</select> ';
-    echo '<button type="submit" class="btn btn-primary">Get Live Tasks</button>';
+    echo '<button type="submit" class="btn btn-primary" id="process_chooser_submit">Get Live Tasks</button>';
     echo '</form>';
 }
+?>
+<script>
+$(document).ready(function() {
+    $('#process_chooser_submit').hide();
+    $("#select_process").change(function() {
+        $('#process_chooser').submit();
+    })
+});
+</script>

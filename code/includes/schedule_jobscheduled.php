3dc32f48e70dae69dab5067808cf306cde2ec3a3
michaelmcmullin <m.mcmullin@outlook.com>
2019-03-24 12:40:46 +0000

    Format the results of scheduling

--- code/includes/schedule_jobscheduled.php
+++ code/includes/schedule_jobscheduled.php
@@ -21,11 +21,11 @@ foreach ($workflow->processes as $process) {
 
 echo '<h4>Step 3: Scheduling</h4>';
 
+echo '<p>Job <strong>'.$job->jobNo.'</strong> has been scheduled as follows:</p>';
 echo '<ul>';
 $sequence = 0;
 foreach ($workflow->processes as $process) {
-    if ($sequence > 6) break; // *******************************************TMP
-    echo '<li>'.$process->name.': '.$process->workflowEstimateTime.'</li>';
+    echo '<li><strong>'.$process->name.'</strong>';
     if (!isset($process->availability)) {
         $time = $process->workflowEstimateTime;
         $process = $processDb->getProcess($process->id);
@@ -34,10 +34,6 @@ foreach ($workflow->processes as $process) {
     $nextTime = date("Y-m-d H:i:s");
     while ($process->workflowEstimateTime > 0) {
         $times = $scheduleDb->getNextSchedule($process, $nextTime);
-        if (count($times) > 0) {
-            echo '<br>Start Time: '.date("Y-m-d H:i:s", strtotime($times[0]));
-            echo '<br>End Time: '.date("Y-m-d H:i:s", strtotime($times[1]));
-        }
         $availableStart = strtotime($times[0]);
         $availableEnd = strtotime($times[1]);
         $availableSeconds = $availableEnd - $availableStart;
@@ -45,72 +41,32 @@ foreach ($workflow->processes as $process) {
         $start = date("Y-m-d H:i:s", $availableStart);
         $end = null;
 
-        echo '<br>Available Seconds: '.$availableSeconds;
-        echo '<br>Required Seconds: ' . $process->workflowEstimateTime;
         if ($availableSeconds > $process->workflowEstimateTime) {
             // Set start and end time
-            echo '<br>We have enough time!';
-            echo '<br>Start Schedule: '.date("Y-m-d H:i:s", $availableStart);
             $endTime = $availableStart+$process->workflowEstimateTime;
-            echo '<br>End Schedule: '.date("Y-m-d H:i:s", $endTime);
             $end = date("Y-m-d H:i:s", $endTime);
         } else {
-            echo '<br>Not enough time, so let us split the process.';
-            echo '<br>Start Schedule: '.date("Y-m-d H:i:s", $availableStart);
-            echo '<br>End Schedule: '.date("Y-m-d H:i:s", $availableEnd).'<hr>';
             $end = date("Y-m-d H:i:s", $availableEnd);
         }
         $scheduleDb->setSchedule($job, $sequence++, $process, $start, $end);
         $process->workflowEstimateTime -= $availableSeconds;
         $nextTime = $end;
+
+        echo '<br>'.date('d/m/Y', strtotime($start));
+        echo ': '.date('H:i', strtotime($start));
+        echo ' - '.date('H:i', strtotime($end));
     }
+    echo "</li>";
 }
 echo '</ul>';
 
-/*
-// Roughwork:
-// 1. Get day of week
-echo getDayNumber("2019-03-24");
-// 2. Download all availabilities for process
-echo '<ul>';
-$p = $processDb->getProcess($workflow->processes[0]->id);
-$p->workflowEstimateTime = $workflow->processes[0]->workflowEstimateTime;
-
-echo 'Process: '.$p->name;
-foreach ($p->availability as $a) {
-    echo '<li>'.$a->getDayName().': '.date("H:i", strtotime($a->startTime)).'</li>';
-}
-echo '</ul>';
-// 3. Get all incomplete jobs for process after given time (null = from now)
-//    Possibly not necessary. Just need to get the next available space.
-$schedules = $scheduleDb->getSchedulesForProcess($p, null);
-echo "Schedule Count: ".count($schedules);
-// 4. Find next available space
-$times = $scheduleDb->getNextSchedule($p, null);
-if (count($times) > 0) {
-    echo '<br>Start Time: '.date("Y-m-d H:i:s", strtotime($times[0]));
-    echo '<br>End Time: '.date("Y-m-d H:i:s", strtotime($times[1]));
-}
-// 5. If available space > process time:
-$availableStart = strtotime($times[0]);
-$availableEnd = strtotime($times[1]);
-$availableSeconds = $availableEnd - $availableStart;
-echo '<br>Available Seconds: '.$availableSeconds;
-echo '<br>Required Seconds: ' . $p->workflowEstimateTime;
-if ($availableSeconds > $p->workflowEstimateTime) {
-    //    * Set start and end time
-    echo '<br>We have enough time!';
-    echo '<br>Start Schedule: '.date("Y-m-d H:i:s", $availableStart);
-    $endTime = $availableStart+$p->workflowEstimateTime;
-    echo '<br>End Schedule: '.date("Y-m-d H:i:s", $endTime);
-    $start = date("Y-m-d H:i:s", $availableStart);
-    $end = date("Y-m-d H:i:s", $endTime);
-    $scheduleDb->setSchedule($job, 0, $p, $start, $end);
+if (strtotime($end) < strtotime($job->deadline)) {
+    echo '<div class="alert alert-success">';
+    echo 'This job is successfully scheduled to be completed before its deadline.';
+    echo '</div>';
 } else {
-    echo '<br>Not enough time, so let us split the process.';
-    // 6. If available space < process time:
-    //    * Fill available time
-    //    * Reduce process time
-    //    * Repeat from 4
+    echo '<div class="alert alert-danger">';
+    echo 'Warning: this job cannot be completed before its deadline of ';
+    echo date('l, jS F, Y', strtotime($job->deadline));
+    echo '</div>';
 }
-*/
